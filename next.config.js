cff4474995c5a5bed0d10f0bdd8e23b817992cc7
Kevin Hung <kevincjhung@gmail.com>
2022-11-22 18:19:07 -0800

    re-init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+    
+const webpack = require('webpack');
+require('dotenv').config();
+
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack: config => {
+    const env = Object.keys(process.env).reduce((acc, curr) => {
+      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+      return acc;
+    }, {});
+    
+    config.plugins.push(new webpack.DefinePlugin(env));
+    
+    return config;
+  }
 }
 
 module.exports = nextConfig
